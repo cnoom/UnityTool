e4bb231c9c9e8ebc5273b6549813f067952ce134
11955 <1195582817@qq.com>
2024-06-14 11:54:45 +0800

    增加单例类

---
+++ CnoomUnityTool/UniUtility/Runtime/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Utils
+{
+    public class Singleton<T>
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => Activator.CreateInstance<T>());
+        
+        protected Singleton()
+        {
+            // 防止外部直接构造实例
+        }
+
+
+        public static T Instance => _instance.Value;
+    }
+}
