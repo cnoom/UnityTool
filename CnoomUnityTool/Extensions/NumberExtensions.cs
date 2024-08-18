b6ceacbac38a02c6a5d06da6fcbd8f9b99a677d6
cnoom <1195582817@qq.com>
2024-08-18 20:32:17 +0800

    新增数字拓展类 新增变换组件子节点相关方法

---
+++ CnoomUnityTool/Extensions/NumberExtensions.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable CheckNamespace
+
+using UnityEngine;
+
+namespace CnoomUnityTool.Extensions
+{
+    public static class NumberExtensions
+    {
+        
+        public static float Abs(this float f)
+        {
+            return Mathf.Abs(f);
+        }
+        
+        public static int Abs(this int f)
+        {
+            return Mathf.Abs(f);
+        }
+        
+        
+    }
+}
--- CnoomUnityTool/Extensions/TransformExtensions.cs
+++ CnoomUnityTool/Extensions/TransformExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace CnoomUnityTool.Extensions
 {
@@ -10,16 +12,38 @@ namespace CnoomUnityTool.Extensions
             return childTransform.gameObject.AddComponent<TMono>();
         }
 
-        public static TMono FindGet<TMono>(this Transform self, string path) where TMono : MonoBehaviour
+        public static T FindGet<T>(this Transform self, string path)
         {
             Transform childTransform = self.Find(path);
-            return childTransform.gameObject.GetComponent<TMono>();
+            return childTransform.gameObject.GetComponent<T>();
         }
 
-        public static TMono FindGetInChildren<TMono>(this Transform self, string path) where TMono : MonoBehaviour
+        public static T FindGetInChildren<T>(this Transform self, string path) 
         {
             Transform childTransform = self.Find(path);
-            return childTransform.gameObject.GetComponentInChildren<TMono>();
+            return childTransform.gameObject.GetComponentInChildren<T>();
         }
+
+        #region Children
+
+        public static void FuncChildren(this Transform self, Action<Transform> action)
+        {
+            foreach (Transform child in self)
+            {
+                action.Invoke(child);
+            }
+        }
+
+        public static void DestroyChildren(this Transform self, float duration = 0)
+        {
+            self.FuncChildren(t => Object.Destroy(t.gameObject, duration));
+        }
+
+        public static void DestroyChildrenImmediate(this Transform self)
+        {
+            self.FuncChildren(t => Object.DestroyImmediate(t.gameObject));
+        }
+
+        #endregion
     }
 }
