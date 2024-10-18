babf3b536db83bcbb95059d9be8de9b4e4a7b3c6
cnoom <1195582817@qq.com>
2024-10-18 15:09:32 +0800

    新增布尔值拓展类，包含布尔值转换整形拓展方法

---
+++ Runtime/Extensions/BoolExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Cnoom.UnityTool.Extensions
+{
+    public static class BoolExtension
+    {
+        public static int Int(this bool value)
+        {
+            return value ? 1 : 0;
+        }
+    }
+}
