c3ccdb7e97f4dfae12c656a62a9e08f656c2532d
cnoom <1195582817@qq.com>
2024-10-18 15:39:36 +0800

    新增dictionary拓展方法 直接获取转换对象

---
+++ Runtime/Extensions/DictionaryExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Cnoom.UnityTool.Extensions
+{
+    public static class DictionaryExtension
+    {
+        /// <summary>
+        /// 获取字典值的子类(减少转换代码)
+        /// </summary>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <returns></returns>
+        public static bool TryGetValueAsChild<T1,T2,T3>(this Dictionary<T1,T2> dictionary, T1 key,out T3 value) where T3 : T2
+        {
+            if(dictionary.TryGetValue(key, out T2 v))
+            {
+                value = (T3)v;
+                return true;
+            }
+            value = default;
+            return false;
+        }
+    }
+}
