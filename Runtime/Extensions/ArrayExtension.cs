25a5ed1c198470307d886478729864a809802267
cnoom <1195582817@qq.com>
2024-10-25 15:15:34 +0800

    新增数组拓展类

---
+++ Runtime/Extensions/ArrayExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cnoom.UnityTool.Extensions
+{
+    public static class ArrayExtension
+    {
+        public static void Clear<T>(this T[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = default;
+            }
+        }
+
+        public static void UniValue<T>(this T[] array,T value)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+        }
+    }
+}
