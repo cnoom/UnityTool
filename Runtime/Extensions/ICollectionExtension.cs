1bd0f34c0cd297dc5b32390b8af9efd8af917be1
cnoom <1195582817@qq.com>
2025-01-14 18:21:46 +0800

    增加ICollection接口拓展下的随机方法

---
+++ Runtime/Extensions/ICollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cnoom.UnityTool.Extensions
+{
+    public static class ICollectionExtension
+    {
+        public static T RandomElement<T>(this ICollection<T> self)
+        {
+            T[] arr = new T[self.Count];
+            self.CopyTo(arr, 0);
+            return arr[Random.Range(0, arr.Length)];
+        }
+    }
+}
