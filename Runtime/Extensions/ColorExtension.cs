bf15f095ece92292f820600462a7f4796ec6f117
cnoom <1195582817@qq.com>
2025-02-01 16:39:41 +0800

    update ColorExtension.cs

---
+++ Runtime/Extensions/ColorExtension.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Cnoom.UnityTool.Extensions
+{
+    public static class ColorExtension
+    {
+        public static Color SetAlpha(this Color self, float alpha)
+        {
+            return new Color(self.r, self.g, self.b, alpha);
+        }
+
+        public static Color SetR(this Color self, float red)
+        {
+            return new Color(red, self.g, self.b, self.a);
+        }
+
+        public static Color SetG(this Color self, float green)
+        {
+            return new Color(self.r, green, self.b, self.a);
+        }
+
+        public static Color SetB(this Color self, float blue)
+        {
+            return new Color(self.r, self.g, blue, self.a);
+        }
+    }
+}
